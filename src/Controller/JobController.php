5c9bff55f8fdc7020ab7c0dd123c895456a5ebd2
amercat25 <tbaptiste25@outlook.com>
2024-10-02 11:16:09 +0200

    form builder

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\Offre;
+use App\Form\OffresType;
+use App\Repository\OffreRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class JobController extends AbstractController
 {
     #[Route('/', name: 'app_job')]
-    public function index(): Response
+    public function index(OffreRepository $offreRepository): Response
     {
         return $this->render('job/index.html.twig', [
-            'controller_name' => 'JobController',
+            'offres' => $offreRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_job_show', requirements: ['id'=>'\d+'])]
+    public function show(Offre $offre): Response
+    {
+        return $this->render('job/show.html.twig', [
+            'offre' => $offre
+        ]);
+    }
+
+    #[Route('/new', name: 'app_job_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+
+        $offre = new Offre();
+
+        $form = $this->createForm(OffresType::class, $offre);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($offre);
+            $em->flush();
+
+            return $this->redirectToRoute('app_job_show', ['id' => $offre->getId()]);
+        }
+
+        return $this->render('job/new.html.twig', [
+          'form' => $form,
         ]);
     }
 }
